58901c62b2e01ac0ae4e518383e5c5e06ad4561b
Michael Porter <mb_porter@outlook.com>
2018-04-07 11:50:41 -0500

    completed User Signup assignment

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import cgi
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+app.config['DEBUG'] = True
 
 
 @app.route("/welcome", methods=['POST'])
 def welcome_user():
-    # look inside the request to figure out what the user typed
     username = request.form['username']
     password = request.form['password']
     vpassword = request.form['vpassword']
@@ -16,17 +15,17 @@ def welcome_user():
 
     uerror = ""
     perror = ""
-    verror = ""
+    verror = "" 
+    eerror = ""
 
-    # if the user typed nothing at all, redirect and tell them the error
-    if (not username) or (username.strip() == ""):
-        uerror = "You must type a valid username."
+    if (not username) or (username.strip() == "") or " " in username or len(username)<3 or len(username)>20:
+        uerror = "You must type a valid username with no spaces between 3 and 20 characters."
 
-    if (not password) or (password.strip() == ""):
-        perror = "You must enter a password."
+    if (not password) or (password.strip() == "") or " " in password or len(password)<3 or len(password)>20:
+        perror = "You must type a valid password with no spaces between 3 and 20 characters."
 
-    if (not vpassword) or (vpassword.strip() == ""):
-        verror = "You must verify your password."
+    if vpassword != password:
+        verror = "The password you entered does not match the Password above."
 
     if len(email) == 0:
         eerror = ""
@@ -34,15 +33,10 @@ def welcome_user():
         eerror = "You must enter a valid email address between 3 and 20 characters with no spaces."
 
     if len(uerror)>0 or len(perror)>0 or len(verror)>0 or len(eerror)>0:
-        return redirect("/?uerror=" + uerror + "&perror=" + perror + "&verror=" + verror + "&eerror=" + eerror)
+        return redirect("/?uerror=" + uerror + "&perror=" + perror + "&verror=" + verror + "&eerror=" + eerror + "&email=" + email + "&username=" + username)
 
-
-    # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
     username_escaped = cgi.escape(username, quote=True)
 
-    # TODO:
-    # Create a template called add-confirmation.html inside your /templates directory
-    # Use that template to render the confirmation message instead of this temporary message below
     return render_template('welcome.html', username=username)
 
 
@@ -52,8 +46,10 @@ def index():
     encoded_perror = request.args.get("perror")
     encoded_verror = request.args.get("verror")
     encoded_eerror = request.args.get("eerror")
+    email = request.args.get("email")
+    username = request.args.get("username")
 
-    return render_template('home.html', uerror=encoded_uerror and cgi.escape(encoded_uerror, quote=True), perror=encoded_perror and cgi.escape(encoded_perror, quote=True), verror=encoded_verror and cgi.escape(encoded_verror, quote=True), eerror=encoded_eerror and cgi.escape(encoded_eerror, quote=True))
+    return render_template('home.html', uerror=encoded_uerror and cgi.escape(encoded_uerror, quote=True), perror=encoded_perror and cgi.escape(encoded_perror, quote=True), verror=encoded_verror and cgi.escape(encoded_verror, quote=True), eerror=encoded_eerror and cgi.escape(encoded_eerror, quote=True), email=email and cgi.escape(email, quote=True), username=username and cgi.escape(username, quote=True))
 
 
 app.run()
